e1d114aeb3c05db4058bbee50b0b2db3c71ba179
Sammy Tabally <sammytabally@gmail.com>
2024-12-15 23:03:29 -0600

    Final

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,34 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Footer.scss'
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../assets/styles/Footer.scss";
 
 function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/Samtab2" target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/sammy-tabally/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>
+        A portfolio designed & built by{" "}
+        <a
+          href="https://github.com/yujisatojr/react-portfolio-template"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sammy Tabally
+        </a>{" "}
+        with ❤️
+      </p>
     </footer>
   );
 }
